f7e93c9b07f06ceb776b4debd72fa523fa9fc8bb
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-07 03:06:26 -0700

    README.md

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
       <Navbar />
       <div className="content">{children}</div>
       <footer>
-        <p>Copyright 2021 LEADconcept</p>
+        <p>@Copyright 2021 LEADconcept</p>
       </footer>
     </div>
   )
